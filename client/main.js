05f36baae20601ef996a5a5b7e401a113fc9809d
Alberto Riolly <albert.r.cahyantara@gmail.com>
2020-06-12 12:57:21 +0700

    Intregation all components

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
-const baseUrl = 'http://localhost:3000/';
-
-let city = 'Pattaya';
+let city = '';
 let country = '';
+const baseUrl = 'http://localhost:3000/';
 
 $(document).ready(function () {
   auth();
@@ -12,11 +11,13 @@ function auth() {
     getDataCovid();
     $('.login-page').hide();
     $('.home-page').show();
+    $('#trip-advisor').hide();
     $('.form-register').hide();
     $('.main-page').show();
   } else {
     $('.login-page').show();
     $('.home-page').hide();
+    $('#trip-advisor').hide();
     $('.form-register').hide();
     $('.main-page').hide();
   }
@@ -40,7 +41,7 @@ function login(event) {
   let password = $('#password-login').val();
   $.ajax({
     method: 'post',
-    url: baseUrl + 'login',
+    url: 'http://localhost:3000/' + 'login',
     data: {
       email,
       password,
@@ -90,7 +91,7 @@ function register(event) {
   event.preventDefault();
   let email = $('#email-register').val();
   let password = $('#password-register').val();
-  console.log(email, password, 'masuk function register');
+  // console.log(email, password, 'masuk function register');
   $.ajax({
     method: 'post',
     url: baseUrl + 'register',
@@ -100,7 +101,7 @@ function register(event) {
     },
   })
     .done((data) => {
-      console.log(data);
+      // console.log(data);
       localStorage.setItem('access_token', data.access_token);
       afterRegister();
     })
@@ -113,16 +114,15 @@ function getDataCovid() {
   let country = 'Indonesia';
   $.ajax({
     method: 'get',
-    url: baseUrl + `/third-APIs/getdata/${country}`,
+    url: baseUrl + `third-APIs/getdata/${country}`,
     headers: {
       access_token: localStorage.access_token,
     },
   })
     .done((data) => {
-      $('#TotalConfirmed').val(data.TotalConfirmed);
-      $('#TotalDeaths').val(data.TotalDeaths);
-      $('#TotalRecovered').val(data.TotalRecovered);
-      console.log(data);
+      $('#TotalConfirmed').text(data.TotalConfirmed);
+      $('#TotalDeaths').text(data.TotalDeaths);
+      $('#TotalRecovered').text(data.TotalRecovered);
     })
     .fail((err) => {
       console.log(err.responseJSON.errors);
@@ -142,94 +142,114 @@ function getweather(city) {
     });
 }
 
-$.ajax({
-  async: true,
-  crossDomain: true,
-  url: `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${city}`,
-  method: 'GET',
-  headers: {
-    'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
-    'x-rapidapi-key': '6b076d3b2bmsh7c1d20c30386137p159568jsna1368603bcf7',
-  },
-})
-  .done(({ data }) => {
-    const advices = {
-      geos: [],
-      things_to_do: [],
-      lodging: [],
-      restaurants: [],
-    };
-    for (let advice of data) {
-      if (advice.result_type == 'geos') {
-        advices.geos.push(advice);
-      } else if (advice.result_type == 'things_to_do') {
-        advices.things_to_do.push(advice);
-      } else if (advice.result_type == 'lodging') {
-        advices.lodging.push(advice);
-      } else if (advice.result_type == 'restaurants') {
-        advices.restaurants.push(advice);
-      }
-    }
+function getPlaceInfo(event) {
+  event.preventDefault;
 
-    for (const category in advices) {
-      for (let [i, advice] of advices[`${category}`].entries()) {
-        if (category == 'geos') {
-          $(`#trip-advisor-${category}`).append(`
-            <div class="jumbotron">
-              <div class="jumbotron-text-container"> 
-                <h1 class="display-4">Welcome to ${advice.result_object.name}!</h1>
-                <p class="lead">
-                  ${advice.result_object.geo_description}
-                </p>
-                <hr class="my-4" />
-                <div class="sub-jumbotron">
-                  <div>
-                    <a class="btn btn-primary btn-lg" href="http://www.google.com/search?q=${advice.result_object.name}" role="button">Explore ${advice.result_object.name}!</a>
-                  </div>
-                  <div class="advice-category">
-                    <p class="btn btn-outline-primary">#${advice.result_object.category.name}
-                    </p>
-                    <p class="btn btn-outline-primary">#${advice.result_object.subcategory[0].name}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          `);
-          $('.jumbotron').css({
-            'background-image': `url(${advice.result_object.photo.images.original.url})`,
-          });
-          city = advice.result_object.name;
-          for (let ancestor of advice.result_object.ancestors) {
-            if (ancestor.subcategory[0].key == 'country')
-              country = ancestor.name;
-          }
-        } else if (
-          category == 'restaurants' ||
-          category == 'lodging' ||
-          category == 'things_to_do'
-        ) {
-          $(`#trip-advisor-${category}`).append(`
-            <div id="${category}-${i}" class="card">
-                <img src="${advice.result_object.photo.images.small.url}" class="card-img-top" alt="">
-                <div class="card-body">
-                  <h5 class="card-title">${advice.result_object.name}</h5>
-                  <p class="card-text">${advice.review_snippet.snippet}</p>
-                </div>
-                <div class="card-footer">
-                  <small class="text-muted">${advice.result_object.address}</small>
-                </div>
-              </div>
-            </div>
-          `);
-        }
-        if (i > 4) break;
-      }
-    }
-    $('#trip-advisor-things_to_do-title').text('Awesome attraction to watch');
-    $('#trip-advisor-lodging-title').text('Classy hotels to rest');
-    $('#trip-advisor-restaurants-title').text('Delicious foods for your meal');
+  city = $('#search-place').val();
+  // $.ajax({
+  //   method: 'get',
+  //   url: `${baseUrl}third-APIs/getPlaceInfo/${city}`,
+  // })
+  $.ajax({
+    async: true,
+    crossDomain: true,
+    url: `https://tripadvisor1.p.rapidapi.com/locations/search?location_id=1&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=${city}`,
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'tripadvisor1.p.rapidapi.com',
+      'x-rapidapi-key': '6b076d3b2bmsh7c1d20c30386137p159568jsna1368603bcf7',
+    },
   })
-  .fail((error) => {
-    console.log('Error get data from trip advisor', error);
-  });
+    .done(({ data }) => {
+      const advices = {
+        geos: [],
+        things_to_do: [],
+        lodging: [],
+        restaurants: [],
+      };
+      for (let advice of data) {
+        if (advice.result_type == 'geos') {
+          advices.geos.push(advice);
+        } else if (advice.result_type == 'things_to_do') {
+          advices.things_to_do.push(advice);
+        } else if (advice.result_type == 'lodging') {
+          advices.lodging.push(advice);
+        } else if (advice.result_type == 'restaurants') {
+          advices.restaurants.push(advice);
+        }
+      }
+
+      for (const category in advices) {
+        $(`#trip-advisor-${category}`).empty();
+        for (let [i, advice] of advices[`${category}`].entries()) {
+          if (category == 'geos') {
+            $(`#trip-advisor-${category}`).append(`
+              <div class="jumbotron">
+                <div class="jumbotron-text-container"> 
+                  <h1 class="display-4">Welcome to ${advice.result_object.name}!</h1>
+                  <p class="lead">
+                    ${advice.result_object.geo_description}
+                  </p>
+                  <hr class="my-4" />
+                  <div class="sub-jumbotron">
+                    <div>
+                      <a class="btn btn-primary btn-lg" href="http://www.google.com/search?q=${advice.result_object.name}" role="button">Explore ${advice.result_object.name}!</a>
+                    </div>
+                    <div class="advice-category">
+                      <p class="btn btn-outline-primary">#${advice.result_object.category.name}
+                      </p>
+                      <p class="btn btn-outline-primary">#${advice.result_object.subcategory[0].name}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `);
+            $('.jumbotron').css({
+              'background-image': `url(${advice.result_object.photo.images.original.url})`,
+            });
+            city = advice.result_object.name;
+            for (let ancestor of advice.result_object.ancestors) {
+              if (ancestor.subcategory[0].key == 'country')
+                country = ancestor.name;
+            }
+          } else if (
+            category == 'restaurants' ||
+            category == 'lodging' ||
+            category == 'things_to_do'
+          ) {
+            $(`#trip-advisor-${category}`).append(`
+              <div id="${category}-${i}" class="card">
+                  <img src="${advice.result_object.photo.images.small.url}" class="card-img-top" alt="">
+                  <div class="card-body">
+                    <h5 class="card-title">${advice.result_object.name}</h5>
+                    <p class="card-text">${advice.review_snippet.snippet}</p>
+                  </div>
+                  <div class="card-footer">
+                    <small class="text-muted">${advice.result_object.address}</small>
+                  </div>
+                </div>
+              </div>
+            `);
+          }
+          if (i > 4) break;
+        }
+      }
+      $('#trip-advisor-things_to_do-title').text('Awesome attraction to watch');
+      $('#trip-advisor-lodging-title').text('Classy hotels to rest');
+      $('#trip-advisor-restaurants-title').text(
+        'Delicious foods for your meal'
+      );
+      $('.login-page').hide();
+      $('#trip-advisor').show();
+      $('.form-register').hide();
+      $('.main-page').hide();
+    })
+    .fail((error) => {
+      console.log('Error get data from trip advisor', error);
+    })
+    .always(() => {
+      citi = '';
+      $('#search-place').val('');
+    });
+}
